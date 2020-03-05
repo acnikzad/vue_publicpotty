a97773ea87822a8cdbf4be9ccfcba1e111512fb5
“Aria <a.c.nikzad@gmail.com>
2020-03-05 09:37:53 -0800

    added first marker

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
-Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_API_KEY,
+    libraries: 'places' // necessary for places input
+  }
+});
+
+axios.defaults.baseURL = 
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map">
+      <gmap-map
+        :center="center"
+        :zoom="zoom"
+        map-type-id="roadmap"
+        style="width: 100%; height: 600px">
+        <gmap-marker
+          :key="index"
+          v-for="(m, index) in markers"
+          :clickable="true"
+          :draggable="false"
+          :name="m.name"
+          :position="m.position"
+          :icon="markerOptions"
+          @click="center=m.position"
+        />
+      </gmap-map>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+const startMarker = ('http://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-Free-PNG-Image.png');
 
 export default {
+  data: function() {
+    return {
+      center: {lat:38.58166, lng:-121.49445},
+      zoom: 13,
+      markers:[],
+      reviews:[],
+      bathrooms: [],
+      markerOptions: {
+        url: startMarker,
+        size: {width: 50, height: 50, f: 'px', b: 'px',},
+        scaledSize: {width: 30, height: 30, f: 'px', b: 'px',}
+      },
+    };
+  },
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+
+  },
+  created: function() {
+    axios.get(`/api/reviews`).then(response => {
+      this.reviews = response.data;
+      console.log(response);
+    });
+
+    axios.get(`/api/bathrooms`).then(response => {
+      this.bathrooms = response.data;
+      console.log(response);
+      console.log(response["data"][0]);
+      this.markers.push({
+        park: response["data"][0]["park"],
+        position: {lat:parseFloat(response["data"][0]["latitude"]), lng:parseFloat(response["data"][0]["longitude"])}});
+      console.log(this.markers);
+    });
+
+
+    
+  },
+};
+
 </script>
